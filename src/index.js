93882e2701e20555754a3d25c6e837ed593f9765
gimly93 <a.lukashevich93@gmail.com>
2017-10-06 18:31:26 +0300

    dsfsdf

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import Search from './components/search';
+import VideoList from './components/video-list';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCJe4SNf96KT8rqMOxVSpj2VN0W_Ge2dyU';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+        YTSearch({key: API_KEY, term: 'surfboards'},
+             (videos) => {
+               this.setState({ videos })
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <Search />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    };
+}
+;
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+    , document.querySelector('.container'));
